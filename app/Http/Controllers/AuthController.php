530ef038eb68023dc9fb80488d91e37ad96e8aee
muhammedmuaz <maazifran03360183798@gmail.com>
2024-03-20 16:06:34 +0500

    status_code Implemented in repherred

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -25,7 +25,7 @@ class AuthController extends Controller
 
         // If validation fails, return error response
         // if ($validator->fails()) {
-            return response()->json(['status' => 'failure', 'message' => 'Error', JsonResponse::HTTP_BAD_REQUEST]);
+            return response()->json(['status' => 'failure', 'message' => 'Error']) ->setStatusCode(Response::HTTP_OK, Response::$statusTexts[Response::HTTP_OK]);
         // }
 
         // $email = $request->email;
